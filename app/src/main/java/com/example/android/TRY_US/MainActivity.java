8d206605bb24b3bf8be32b06c93ca13389ce8a04
fujitayuya <s8272@g.maizuru-ct.ac.jp>
2017-09-06 23:31:53 +0900

    [ADD] History List View

--- app/src/main/java/com/example/android/TRY_US/MainActivity.java
+++ app/src/main/java/com/example/android/TRY_US/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.android.TRY_US;
+import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioFormat;
@@ -16,11 +17,13 @@ import android.speech.tts.UtteranceProgressListener;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -32,11 +35,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends ListActivity
         implements View.OnClickListener, TextToSpeech.OnInitListener, OnCheckedChangeListener{
 
+    ArrayAdapter<String> adapter;
     public static final int SPEECH_RECOG_REQUEST = 42;
-    TextView fftText;
+   // TextView fftText;
     EditText editText;
     int SAMPLING_RATE = 44100;
     // FFTのポイント数
@@ -47,6 +51,7 @@ public class MainActivity extends AppCompatActivity
     boolean fftBool=false;
     AudioManager mAudioManager;
     double dB_baseline = Math.pow(2, 15) * FFT_SIZE * Math.sqrt(2);
+
 
     // 分解能の計算
     double resol = ((SAMPLING_RATE / (double) FFT_SIZE));
@@ -59,10 +64,25 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView textView = (TextView)findViewById(R.id.FFTtext);
+        //ArrayAdapterオブジェクト生成
+        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        //Buttonオブジェクト取得
+        Button btn=(Button)findViewById(R.id.btn);
+        //ListAdapterセット
+        setListAdapter(adapter);
+        // ここで1秒間スリープし、スプラッシュを表示させたままにする。
+            try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+        // スプラッシュthemeを通常themeに変更する
+        setTheme(R.style.AppTheme);
+        setContentView(R.layout.activity_main);
 
 
-        textView.setText("fft" + "周波数："+ String.valueOf(freq) + " [Hz] 音量：" + String.valueOf(vol));
+        //final TextView textView = (TextView)findViewById(R.id.FFTtext);
+
+        //textView.setText("fft" + "周波数："+ String.valueOf(freq) + " [Hz] 音量：" + String.valueOf(vol));
         speechRecogStuff();
         tts = new TextToSpeech(this, this);
         bufSize = AudioRecord.getMinBufferSize(SAMPLING_RATE,
@@ -92,8 +112,9 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+
         editText = (EditText) findViewById(R.id.edit_text);
-        fftText = (TextView) findViewById(R.id.FFTtext);
+    //    fftText = (TextView) findViewById(R.id.FFTtext);
         // AudioRecordの作成
         audioRec = new AudioRecord(MediaRecorder.AudioSource.MIC,
                 SAMPLING_RATE, AudioFormat.CHANNEL_IN_MONO,
@@ -101,41 +122,24 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-
-/*
-    public void onClickButton(View v) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // マルチスレッドにしたい処理 ここから
-
-                final String result = getMessage(); // 何かの処理
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        msgView.setText(result); // 画面に描画する処理
-                    }
-                });
-
-                // マルチスレッドにしたい処理 ここまで
-            }
-        }).start();
-    }
-*/
     @Override
     public void onInit(int status) {
         // TTS初期化
         if (TextToSpeech.SUCCESS == status) {
-          //  Log.d(, "initialized");
+            //  Log.d(, "initialized");
         } else {
-         //   Log.e(TAG, "faile to initialize");
+            //   Log.e(TAG, "faile to initialize");
         }
     }
+
+
 
     @Override
     public void onClick(View v) {
         speechText();
     }
+
+
 
     private void shutDown(){
         if (null != tts) {
@@ -143,9 +147,6 @@ public class MainActivity extends AppCompatActivity
             tts.shutdown();
         }
     }
-
-
-
 
 
 
@@ -176,6 +177,7 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+
     // 読み上げのスピード
     private void setSpeechRate(float rate){
         if (null != tts) {
@@ -183,12 +185,14 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+
     // 読み上げのピッチ
     private void setSpeechPitch(float pitch){
         if (null != tts) {
             tts.setPitch(pitch);
         }
     }
+
 
     // 読み上げの始まりと終わりを取得
     private void setTtsListener(){
@@ -201,138 +205,132 @@ public class MainActivity extends AppCompatActivity
                 @Override
                 public void onDone(String utteranceId)
                 {
-               //     Log.d(TAG,"progress on Done " + utteranceId);
+                    //     Log.d(TAG,"progress on Done " + utteranceId);
                 }
 
                 @Override
                 public void onError(String utteranceId)
                 {
-                 //   Log.d(TAG,"progress on Error " + utteranceId);
+                    //   Log.d(TAG,"progress on Error " + utteranceId);
                 }
 
                 @Override
                 public void onStart(String utteranceId)
                 {
-                   // Log.d(TAG,"progress on Start " + utteranceId);
+                    // Log.d(TAG,"progress on Start " + utteranceId);
                 }
 
             });
             if (listenerResult != TextToSpeech.SUCCESS)
             {
-             //   Log.e(TAG, "failed to add utterance progress listener");
+                //   Log.e(TAG, "failed to add utterance progress listener");
             }
         }
         else {
-           // Log.e(TAG, "Build VERSION is less than API 15");
+            // Log.e(TAG, "Build VERSION is less than API 15");
         }
 
     }
 
-        private android.speech.SpeechRecognizer speechRecognizer;
-        private RecognitionListener myListener = new RecognitionListener() {
-            public int bufferCounter = 0;
+
+    private android.speech.SpeechRecognizer speechRecognizer;
+    private RecognitionListener myListener = new RecognitionListener() {
+        public int bufferCounter = 0;
 
 
-            @Override
-            public void onReadyForSpeech(Bundle params) {
-                speechRecogStatus.setText("録音開始");
-            }
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            speechRecogStatus.setText("録音開始");
+        }
 
-            @Override
-            public void onBeginningOfSpeech() {
-                speechRecogStatus.setText("認識中");
-                bufferCounter = 0;
-            }
+        @Override
+        public void onBeginningOfSpeech() {
+            speechRecogStatus.setText("認識中");
+            bufferCounter = 0;
+        }
 
-            @Override
-            public void onRmsChanged(float rmsdB) {
-            }
+        @Override
+        public void onRmsChanged(float rmsdB) {
+        }
 
-            @Override
-            public void onBufferReceived(byte[] buffer) {
-                bufferCounter++;
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+            bufferCounter++;
 //            speechRecogStatus.setText("onBufferReceived() - " + bufferCounter);
-            }
+        }
 
-            @Override
-            public void onEndOfSpeech() {
-                speechRecogStatus.setText("認識終了");
-                bufferStatus.setText("counter = " + bufferCounter);
-            }
+        @Override
+        public void onEndOfSpeech() {
+            speechRecogStatus.setText("認識終了");
+        }
 
-            @Override
-            public void onError(int error) {
-                String errorString = getErrorString(error);
-                speechRecogStatus.setText("エラー = " + errorString);
-                try{
-                    Thread.sleep(2000);
-                }catch (InterruptedException e){}
-                startSpeechRecog();
-            }
+        @Override
+        public void onError(int error) {
+            String errorString = getErrorString(error);
+            speechRecogStatus.setText("エラー = " + errorString);
+            try{
+                Thread.sleep(100);
+            }catch (InterruptedException e){}
+            startSpeechRecog();
+        }
 
-            private String getErrorString(int error) {
-                switch (error) {
-                    case SpeechRecognizer.ERROR_AUDIO:
-                        return "オーディオエラー";
-                    case SpeechRecognizer.ERROR_CLIENT:
-                        return "クライアントエラー";
-                    case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
-                        return "権限が必要";
-                    case SpeechRecognizer.ERROR_NETWORK:
-                    case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
-                    case SpeechRecognizer.ERROR_SERVER:
-                        return "サーバー/ネットワークエラー";
-                    case SpeechRecognizer.ERROR_NO_MATCH:
-                        return "マッチなし";
-                    case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
-                        return "apparently, recognizer busy";
-                    case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
-                        return "タイムアウト";
-                }
-                return "";
+        private String getErrorString(int error) {
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    return "オーディオエラー";
+                case SpeechRecognizer.ERROR_CLIENT:
+                    return "クライアントエラー";
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    return "権限が必要";
+                case SpeechRecognizer.ERROR_NETWORK:
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                case SpeechRecognizer.ERROR_SERVER:
+                    return "サーバー/ネットワークエラー";
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    return "マッチなし";
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    return "apparently, recognizer busy";
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    return "タイムアウト";
             }
+            return "";
+        }
 
-            @Override
-            public void onResults(Bundle results) {
-                speechRecogStatus.setText("結果");
-                String finalResult = StringUtils.join(results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION), ',');
-                speechRecogResult.setText("" + finalResult);
-                TextView recently1 = (TextView) findViewById(R.id.recently_speech_recog_result1);
-                TextView recently2 = (TextView) findViewById(R.id.recently_speech_recog_result2);
-                TextView recently3 = (TextView) findViewById(R.id.recently_speech_recog_result3);
-                recently3.setText(recently2.getText());
-                recently2.setText(recently1.getText());
-                recently1.setText(results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION).get(0));
-                try{
-                    Thread.sleep(2000);
-                }catch (InterruptedException e){}
-                startSpeechRecog();
-            }
+        @Override
+        public void onResults(Bundle results) {
+            speechRecogStatus.setText("結果");
+            String finalResult = StringUtils.join(results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION), ',');
+            speechRecogResult.setText("" + finalResult);
+            ListView listView=(ListView) findViewById(android.R.id.list);
+            adapter.add(results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION).get(0));
+            listView.setSelection(listView.getCount()-1);
+            try{
+                Thread.sleep(100);
+            }catch (InterruptedException e){}
+            startSpeechRecog();
+        }
 
-            /*@Override
-            public void onPartialResults(Bundle partialResults) {
-                speechRecogStatus.setText("リアルタイム結果");
-                processResults(partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION));
-            }*/
-            @Override
-            public void onPartialResults(Bundle partialResults) {
-                processResults(partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION));
-            }
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+            processResults(partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION));
+        }
 
-            @Override
-            public void onEvent(int eventType, Bundle params) {
-                speechRecogStatus.setText("onEvent");
-            }
-        };
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+            speechRecogStatus.setText("onEvent");
+        }
+    };
+
 
     private void processResults(ArrayList<String> speechRecogResults) {
         String finalResult = StringUtils.join(speechRecogResults, ',');
         speechRecogResult.setText(speechRecogResults.get(0));
     }
 
+
     private TextView speechRecogResult;
     private TextView speechRecogStatus;
-    private TextView bufferStatus;
+
 
     @Override
     protected void onDestroy(){
@@ -341,16 +339,17 @@ public class MainActivity extends AppCompatActivity
     }
 
 
+
     private void speechRecogStuff() {
         speechRecogResult = (TextView) findViewById(R.id.speech_recog_result);
         speechRecogStatus = (TextView) findViewById(R.id.speech_recog_status_text);
-        bufferStatus = (TextView) findViewById(R.id.buffer_status);
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
         speechRecognizer.setRecognitionListener(myListener);
         if (fftBool==false) {
             startSpeechRecog();
         }
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -364,9 +363,9 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+
     private void startSpeechRecog() {
         speechRecogResult.setText(null);
-        bufferStatus.setText(null);
         Intent recogIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
                 .putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH)
                 .putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
@@ -375,6 +374,7 @@ public class MainActivity extends AppCompatActivity
         //startActivityForResult(recogIntent, SPEECH_RECOG_REQUEST);
         speechRecognizer.startListening(recogIntent);
     }
+
 
     public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked){
         if(isChecked==true){
@@ -458,6 +458,7 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+
     Handler handler = new Handler();
     @Override
     protected void onResume() {
@@ -465,6 +466,7 @@ public class MainActivity extends AppCompatActivity
         speechRecognizer.destroy();
         speechRecogStuff();
     }
+
 
     @Override
     protected void onPause(){
---
+++ app/src/test/java/com/example/android/TRY_US/subActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.android.TRY_US;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by fujitayuuya on 2017/09/06.
+ */
+public class subActivityTest {
+
+}
