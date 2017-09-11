66a4ce99e4c131e84ed21827018d0b166d23f4e0
Kanta Demizu <demizu.1203@gmail.com>
2017-09-11 09:11:15 +0900

    [CHANGED] List View Editable

--- app/src/main/java/com/example/android/TRY_US/SubActivity.java
+++ app/src/main/java/com/example/android/TRY_US/SubActivity.java
@@ -5,28 +5,40 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class SubActivity extends AppCompatActivity {
     InputStream is = null;
     BufferedReader br = null;
     String text = "";
     String msg;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("定型文");
+        UtilCommon editable = (UtilCommon)getApplication();
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //ListViewオブジェクトの取得
@@ -55,18 +67,44 @@ public class SubActivity extends AppCompatActivity {
         } catch (Exception e){
             // エラー発生時の処理
         }
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ListView listView = (ListView)parent;
-                msg = (String)listView.getItemAtPosition(position);
-                        Intent intent_ret = new Intent();
-                        intent_ret.putExtra("RESULT", msg);
-                        setResult(RESULT_OK, intent_ret);
-                        finish();
-                    }
-        });
+        if (!editable.getGlobal()) {
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    ListView listView = (ListView) parent;
+                    msg = (String) listView.getItemAtPosition(position);
+                    Intent intent_ret = new Intent();
+                    intent_ret.putExtra("RESULT", msg);
+                    setResult(RESULT_OK, intent_ret);
+                    finish();
+                }
+            });
+        }else if(editable.getGlobal()){
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    final ListView listView = (ListView) parent;
+                    msg = (String) listView.getItemAtPosition(position);
+                    setContentView(R.layout.edit_text);
+                    final EditText editText=(EditText) findViewById(R.id.edit_text);
+                    editText.setText(msg);
+                    editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                        @Override
+                        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                            boolean handled = false;
+                            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                                // ... なんかの処理 ...
+                                msg=editText.getText().toString();
+                                listView.getItemAtPosition(0);
+                                handled = true;
+                                finish();
+                            }
+                            return handled; // このメソッド中でアクションを消化したら true を返す。
+                        }
+                    });
+                }
+            });
+        }
 
         listView.setAdapter(adapter);
     }
